85c9df789ab4d185719c61aeaef8dae086b81f5e
Banti Gheneti <banti@gheneti.com>
2019-01-15 22:56:24 -0500

    working MIQP w/ line and spline cleaned up + results

--- trajectory_planner.py
+++ trajectory_planner.py
@@ -102,7 +102,7 @@ class BoatConfigurationPlanning(object):
                 for i in range(self.boat.num_inputs):
                     mp.AddLinearConstraint(u0[i] >= -self.boat.max_u)
                     mp.AddLinearConstraint(u0[i] <=  self.boat.max_u)    
-                    
+
         print '%f seconds' % (time.time()-start)
         
                 
@@ -126,127 +126,106 @@ class BoatConfigurationPlanning(object):
         return boats_S, boats_U, time_array[-1], mp, result, solve_time
 
 
-    def compute_spline_trajectory(self, minimum_time, maximum_time, state_initial, state_final, input_position_cost=False, input_angle_cost=False, dif_states_cost=False, states_initialization=None, step_initialization=None, fix_initialization_inds=None, in_hull=None, on_edge=None, N=100, slack=1e-3):
+    def compute_spline_trajectory(self, S_initial, S_final, opt_position=True, opt_angle=True, S_initialization=None, in_hull=None, on_edge=None, S_fix_inds=None, N=15):
                 
         start = time.time()
         
         #Check variable dimensions
         ##########################
         
-        assert state_initial.shape==state_final.shape   # Same number of boats, state vars for final and initial states
+        assert S_initial.shape==S_final.shape   # Same number of boats, state vars for final and initial states
+        assert S_initial.shape[0]==1
         
         #Initialize Optimization Variables
         ##################################       
         
-        mp = AugmentedMathematicalProgram()
-                           
-        num_boats = state_initial.shape[0]
-        
-        #initialize time variables
-        
-        finish_time = maximum_time
-        step = finish_time/float(N)
-        
-        T = N+1
-        time_array = np.arange(0.0, T)*step
+        mp = AugmentedMathematicalProgram()                
+        T  = N+1
 
         #initialize state variables
-
-        problem_state_initial = self.boat.toProblemState(state_initial)
-        problem_state_final   = self.boat.toProblemState(state_final)
+        problem_S_initial = self.boat.toProblemState(S_initial)
+        problem_S_final   = self.boat.toProblemState(S_final)
         
-        boats_S = mp.states(N+1, problem_state_initial, states_final=problem_state_final)        
-        boats_U = mp.inputs(N, self.boat.num_inputs, num_boats=num_boats)
+        boats_S = mp.states(N+1, problem_S_initial, states_final=problem_S_final)
+        boats_U = mp.inputs(N, self.boat.num_inputs)
         
-        if states_initialization is not None:
-            problem_states_initialization = self.boat.toProblemStates(states_initialization)
+        if S_initialization is not None:
+            problem_S_initialization = self.boat.toProblemStates(S_initialization)
+            mp.SetInitialGuess(boats_S[0], problem_S_initialization[0])
             
-            for i in range(num_boats):
-                mp.SetInitialGuess(boats_S[i], problem_states_initialization[i])
-            if fix_initialization_inds is not None:
-                mp.fix_initialization(boats_S, states_initialization, fix_initialization_inds)
+            if S_fix_inds is not None:
+                mp.fix_initialization(boats_S, problem_S_initialization, S_fix_inds)
         
-        opt_hull = False
-        
-        if in_hull is None:
-            opt_hull=True
-            in_hull = mp.NewBinaryVariables(T-1, len(self.boat.hull_path), "hull")
+        # integer variables
+        opt_hull = in_hull is None or on_edge is None
+        if opt_hull:
+            in_hull  = mp.NewBinaryVariables(T-1, len(self.boat.hull_path), "hull")
+            on_edge  = mp.NewBinaryVariables(T-2, "edge")
             
-        if on_edge is None:
-            on_edge = mp.NewBinaryVariables(T-2, 1, "edge")            
+            mp.add_equal_constraints(np.sum(in_hull, axis=1), np.ones(in_hull.shape[0]))
+            
+        angle_mod = np.zeros((N,2)) 
+        #mp.add_leq_constraints(angle_mod[:,0]+angle_mod[:,1],np.ones(N))
 
-        print bold('INITIALIZED %d %s boats') % (num_boats, self.boat.__class__.__name__) +'\nboats_S:%s, boats_U:%s, time_array:%s' % (boats_S.shape, boats_U.shape, time_array.shape)
+        print bold('INITIALIZED %s ') % (self.boat.__class__.__name__) + \
+              '\nboats_S:%s, boats_U:%s' % (boats_S.shape, boats_U.shape)    
         print 'Number of decision vars', mp.num_vars()        
         print '%f seconds' % (time.time()-start)
-        
-        ##Calculate Quadratic B-Spline Parameters
-        
-        M = 0.5 * np.array([[1, 1, 0],[-2, 2, 0],[1, -2, 1]])
-        arr = np.array([[0, 0, 2]])        
-        Q  = arr.T.dot(arr)
-        deriv_mat = M.T.dot(Q.dot(M))
+
 
         #Costs
         ######
         
-        print bold('ADDING COSTS')
-        
+        print bold('ADDING COSTS')  
         start=time.time()
         
-        if input_position_cost:
-            for k in range(T):
-                P = np.zeros((3,2),boats_S.dtype)
-                if k==0:
-                    P[0] = P[1] = boats_S[0,0,:2]
-                    P[2] = boats_S[0,k+1,:2]
-                elif k>N-1:
-                    P[0] = boats_S[0,k-1,:2]
-                    P[1] = P[2] = boats_S[0,N,:2]
-                else:
-                    P=boats_S[0,k-1:k+2,:2]
-                print P
-                mp.AddQuadraticCost(np.sum(np.multiply(deriv_mat.dot(P),P)))
-                
-        if input_angle_cost:
-            mp.AddQuadraticCost(np.sum(boats_U[0,:,2:]**2/1000))
-                                
+        if opt_position:
+            self.boat.add_input_position_cost(boats_S, boats_U, mp)
+
+        if opt_angle:
+            self.boat.add_input_angle_cost(boats_S, boats_U, mp)
+
+        print 'Number of costs', len(mp.quadratic_costs())                                  
         print '%f seconds' % (time.time()-start)
+
         
         #Constraints
         ############
         
         print bold('ADDING CONSTRAINTS')        
         start=time.time()
+        
+        if opt_position:
+            self.boat.add_position_collision_constraints(boats_S, in_hull, on_edge, mp)
 
-        self.boat.add_collision_constraints(mp, boats_S, in_hull, on_edge, opt_hull=opt_hull)
-        
-        angle_mod = mp.NewBinaryVariables(N, 2, "angle_mod")
-        self.boat.angle_mod = angle_mod
-        mp.add_leq_constraints(angle_mod[:,0]+angle_mod[:,1],np.ones(N))
-        
-        #angular velocity update
-        mp.add_equal_constraints(boats_S[0,1:,5],boats_S[0,:-1,5]+.50*boats_U[0,:,2]+.50*boats_U[0,:,3],linear=True)
-        
-        #angle update
-        mp.add_equal_constraints(boats_S[0,1:,2],boats_S[0,:-1,2]+(angle_mod[:,0]-angle_mod[:,1])*360+\
-                                 boats_S[0,:-1,5]+.375*boats_U[0,:,2]+.125*boats_U[0,:,3],linear=True)
+        if opt_angle:
+            self.boat.add_angle_collision_constraints(boats_S, in_hull, mp)
 
+        if opt_position or opt_angle:
+            self.boat.add_transition_constraints(boats_S, boats_U, angle_mod, mp)
+
+        if opt_hull:
+            self.boat.add_integer_constraints(in_hull, on_edge, angle_mod, mp)
+            
+        print 'Number of constraints', len(mp.linear_constraints())                      
         print '%f seconds' % (time.time()-start)
-                
+
+        
         #Calculate Solution 
         ###################
         
         start=time.time()
-
         print bold('PLANNING')
+        
         result = mp.Solve()
-        print result
         solve_time = time.time()-start
+        
+        print result
         print '%f seconds' % (solve_time)
         print
         
         boats_U = np.array([mp.GetSolution(U) for U in boats_U])
-        boats_S = self.boat.toGlobalStates(np.array([mp.GetSolution(S) for S in boats_S]), state_initial)
+        boats_S = self.boat.toGlobalStates(np.array([mp.GetSolution(S) for S in boats_S]), S_initial)
                 
         if opt_hull:
             in_hull = mp.GetSolution(in_hull).reshape(in_hull.shape)
@@ -257,4 +236,3 @@ class BoatConfigurationPlanning(object):
 def write_experiment(boat, boats_S, boats_U, label):
     with open('results/path_'+label+'.pickle', 'wb') as f:
         pickle.dump([boat,boats_S,boats_U], f)
-    
